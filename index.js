2ea62c3af00dd921f99f52fde59d498cdac4948b
Neil Rutledge <neilrutledgephoto@gmail.com>
2018-10-14 15:54:22 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log('successfully connected to Mongo'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+app.use(cookieSession({
+    // @TODO: reduce cookie maxAge after testing
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: keys.cookieSessionKeys
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
